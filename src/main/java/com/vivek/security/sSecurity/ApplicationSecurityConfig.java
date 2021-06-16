0b94e011598fdfdbdf2fc967bbafda993a360b66
SinghVivek96 <itsvivek96@gmail.com>
2021-06-16 21:25:23 +0530

    Roles Based Permissions

--- src/main/java/com/vivek/security/sSecurity/ApplicationSecurityConfig.java
+++ src/main/java/com/vivek/security/sSecurity/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vivek.security.sSecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,18 +9,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
-                .permitAll()
+                .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -30,10 +35,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected UserDetailsService userDetailsService() {
         UserDetails vivek = User.builder()
-                                .username("vivek")
-                                .password("vivek")
-                                .roles("STUDENT")
+                                .username("viveks")
+                                .password(passwordEncoder.encode("viveks"))
+                                .roles(ApplicationUserRole.STUDENT.name())
                                 .build();
+
+        UserDetails ADMIN = User.builder()
+                                .username("admin")
+                                .password(passwordEncoder.encode("admin"))
+                                .roles(ApplicationUserRole.ADMIN.name())
+                                .build();
+
         return new InMemoryUserDetailsManager(vivek);
     }
 }
